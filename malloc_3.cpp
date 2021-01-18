9097cbcc78e74f4ce3b3cc462adee4f24a6e3926
drozen1 <drozen@campus.technion.ac.il>
2021-01-18 18:32:52 +0000

    push part 3

---
+++ malloc_3.cpp
@@ -0,0 +1,171 @@
+//
+// Created by student on 1/16/21.
+//
+#include <cstring>
+#include <cmath>
+#include "stddef.h"
+#include <iostream>
+#include <unistd.h>
+
+
+void* first_sbrk = NULL;
+void* first_mmap = NULL;
+
+struct MallocMetadata{
+    size_t size;
+    bool is_free;
+    bool is_mmap;
+    MallocMetadata* next;
+    MallocMetadata* prev;
+};
+
+void* smalloc(size_t size){
+    if( size==0 || size> (pow(10,8))){
+        return NULL;
+    }
+    if(size>128 * 1024){
+        mmap_hanler(size); #TODO
+    }
+    MallocMetadata* pos=(MallocMetadata *)first;
+    MallocMetadata* last=NULL;
+    while(pos!=NULL){
+        if(pos->is_free && pos->size>=size){
+            if(pos->size-size-size(MallocMetadata)>=128) {
+                pos->is_free = false;
+                cutblock(pos, size); #TODO
+                return (pos + 1);
+            }
+            else{
+                pos->is_free = false;
+                return (pos + 1);
+            }
+        }
+        last=pos;
+        pos=pos->next;
+    }
+    size_t sbrk_size=size;
+    if(last->is_free){ //TODO:countionue from this ponit
+        sbrk_size-=(last->size+sizeof(MallocMetadata));
+    }
+    void* ret= sbrk(sbrk_size+sizeof(MallocMetadata));
+    if (ret== (void*)-1){
+        return NULL;
+    }
+    MallocMetadata* new_block= (MallocMetadata*)ret;
+    new_block->size=size;
+    new_block->is_free=false;
+    new_block->next=NULL;
+    if(first==NULL){
+        new_block->prev=NULL;
+        first=(void*)new_block;
+        return (new_block+1);
+    }
+    last->next=new_block;
+    new_block->prev=last;
+    return (new_block+1);
+}
+
+void* scalloc(size_t num, size_t size){
+    if( size==0 || size> (pow(10,8))){
+        return NULL;
+    }
+    void * ret_block = smalloc(size*num);
+    if(ret_block == NULL) return NULL;
+    return memset(ret_block, 0, size*num);
+
+}
+
+void sfree(void* p){
+    if(p!=NULL){
+        MallocMetadata* pos = ((MallocMetadata*)((unsigned long)p - sizeof(MallocMetadata)));
+        if(!pos->is_free){
+            pos->is_free= true;
+        }
+    }
+}
+
+void* srealloc(void* oldp, size_t size){
+    if( size==0 || size> (pow(10,8))){
+        return nullptr;
+    }
+    if(oldp!= nullptr) {
+        MallocMetadata* pos=(MallocMetadata *)oldp;
+        pos = pos - 1;
+        if (pos->size >= size) {
+            return oldp;
+        }
+    }
+    void* newp= smalloc(size);
+    if(newp==nullptr){
+        return nullptr;
+    }
+    if(oldp!=nullptr) {
+        memmove(newp, oldp, size);
+        sfree(oldp);
+    }
+    return newp;
+}
+
+size_t _num_free_blocks(){
+    if( first== nullptr){
+        return 0;
+    }
+    MallocMetadata* pos= (MallocMetadata *)first;
+    size_t counter=0;
+    while(pos!= nullptr){
+        if(pos->is_free){
+            counter++;
+        }
+        pos=pos->next;
+    }
+    return counter;
+}
+
+size_t _num_free_bytes(){
+    if( first== nullptr){
+        return 0;
+    }
+    MallocMetadata* pos= (MallocMetadata *)first;
+    size_t counter=0;
+    while(pos!= nullptr){
+        if(pos->is_free){
+            counter+= pos->size; ///maybe not used size of not free?
+        }
+        pos=pos->next;
+    }
+    return counter;
+}
+
+size_t _num_allocated_blocks(){
+    if( first== nullptr){
+        return 0;
+    }
+    MallocMetadata* pos= (MallocMetadata *)first;
+    size_t counter=0;
+    while(pos!= nullptr){
+        counter+=1;
+        pos=pos->next;
+    }
+    return counter;
+}
+
+size_t _num_allocated_bytes(){
+    if( first== nullptr){
+        return 0;
+    }
+    MallocMetadata* pos= (MallocMetadata *)first;
+    size_t counter=0;
+    while(pos!= nullptr){
+        counter+=pos->size;
+        pos=pos->next;
+    }
+    return counter;
+}
+size_t _num_meta_data_bytes(){
+    return _num_allocated_blocks()*sizeof(MallocMetadata);
+}
+
+
+size_t _size_meta_data(){
+    return sizeof (MallocMetadata);
+}
